3cd121528c1d90e62ebd7da1424a6d95dcf95571
Dylan Cordoni <email": null>
2017-03-24 10:34:01 -0700

    Pulls models and migrations

---
+++ db/migrate/20170323155618_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :password_hash
+
+      t.integer :mile_radius
+
+      t.timestamps
+    end
+  end
+end
